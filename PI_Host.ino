0f217b01d447bbf0967875c45068749266af01f6
James Hughes <jhughes1010@gmail.com>
2022-05-24 04:43:21 -0600

    setIO now functioning

--- PI_Host.ino
+++ PI_Host.ino
@@ -12,59 +12,15 @@ void setup()
   Serial.begin(115200);
   //Serial.println(NAME);
 
-  //setIO();
-  
+  setIO();
+
   Wire.begin();
-  //Wire.onReceive(receiveEvent);
   //debugln("setup completed");
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(AUDIO, OUTPUT);
-  tone(AUDIO,440);
+  tone(AUDIO, 440);
 }
 
 void loop()
 {
-  /*
-  int size = 6;
-  byte lowByte, highByte;
-  unsigned int countArray[6];
-  int position;
-  //unsigned int countArray[6];
-  delay(2000);
 
-  if (count % 2 == 0)
-  {
-    for (position = 0 ; position < size; position++)
-    {
-      countArray[position] = 0x8000;
-    }
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.println("LOW");
-  }
-  else
-  {
-    for (position = 0 ; position < size; position++)
-    {
-      countArray[position] = 0x0002;
-    }
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("HIGH");
-  }
-  count++;
-
-  //Send I2C
-  Wire.beginTransmission(8);
-
-
-  for (position = 0 ; position < size; position++)
-  {
-    Serial.println(countArray[position]);
-    lowByte = byte (countArray[position] & 0xff);
-    highByte = byte ((countArray[position] >> 8) & 0xff);
-    Wire.write(lowByte);
-    Wire.write(highByte);
-  }
-  Wire.endTransmission();
-  */
 }
--- utility.ino
+++ utility.ino
@@ -1,16 +1,12 @@
-/*void setIO(void)
+void setIO(void)
 {
-  pinMode(txPin, OUTPUT);           // Set TX pin to output mode
-  pinMode(mainSamplePin, OUTPUT);   // Set main sample pin to output mode
-  pinMode(efeSamplePin, OUTPUT);    // Set EFE sample pin to output mode
-
-  //LED and A1 defined
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(A1, INPUT);
+  pinMode(AUDIO, OUTPUT);
+
   //Pulse LED for 1000ms to indicate boot
   digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
   digitalWrite(LED_BUILTIN, HIGH);
   delay(1000);
   digitalWrite(LED_BUILTIN, LOW);
-}*/
+}
